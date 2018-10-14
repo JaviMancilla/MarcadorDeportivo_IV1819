bba9ec081a19277f638a07c086761bdf5ada5d51
JaviMancilla <javierelmancilla@hotmail.com>
2018-10-15 01:28:14 +0200

    Modificando version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='travistest',
     packages=['travistest'],
-    version='1.2',
+    version='1.2.1',
     description='Hitos del proyecto pasando test con Travis',
     url='https://github.com/JaviMancilla/MarcadorDeportivo_IV1819.git',    
 )
