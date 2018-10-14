e8c47a6d45360d4700b19ad1b65dea4cb233a37a
JaviMancilla <javierelmancilla@hotmail.com>
2018-10-14 19:52:18 +0200

    Borrando carpeta de test

--- marcador/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='travistest',
-    packages=['travistest'],
-    version='1.2',
-    description='Hitos del proyecto pasando test con Travis',
-    url='https://github.com/lqdev/TravisTest',    
-)
